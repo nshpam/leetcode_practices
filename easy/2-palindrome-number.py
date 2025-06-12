9f72fe3ce928fc588f89e25bce18e7c4cc54e535
NSHPAM (Sirapa Sangna) <97942535+nshpam@users.noreply.github.com>
2025-06-12 22:55:28 +0700

    Create 2-palindrome-number.py

---
+++ easy/2-palindrome-number.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # 1st try
+        # time => O(log n)
+        # space => O(log n)
+        # palindrome = str(x)[::-1] # string reverse
+        # if str(x) == palindrome:
+        #     return True
+        # return False
+
+        # 2nd try => solve without converting the integer to a string
+        # time => O(log n)
+        # space => O(1)
+        # new_x = 0
+        # copy_x = x
+        # while copy_x>0:
+        #     # remove remainder
+        #     remainder = copy_x % 10
+        #     # remove new x
+        #     copy_x = copy_x // 10
+        #     # create reverse
+        #     new_x = new_x * 10 + remainder
+        # if new_x == x:
+        #     return True
+        # return False
+
+        # 3rd => half reversal : faster than integer reversal
+        # time => O(log n)
+        # space => O(1)
+        reverse_half = 0
+        # check if the last digit is 0 or negative value
+        if (x%10 == 0 and x!=0) or x<0:
+            return False
+
+        while x>reverse_half:
+            remainder = x%10
+            reverse_half = reverse_half*10 + remainder
+            x = x//10
+            # print(remainder, reverse_half, x)
+        
+        return x==reverse_half or x==reverse_half//10
