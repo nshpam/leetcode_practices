1b978c8542421f29a3b7b5f98598f037e76c066c
NSHPAM (Sirapa Sangna) <97942535+nshpam@users.noreply.github.com>
2025-06-15 19:34:17 +0700

    Update 1-two-sum.py

--- easy/1-two-sum.py
+++ easy/1-two-sum.py
@@ -27,3 +27,38 @@ class Solution:
             if complement in nums_to_index:
                 return [nums_to_index[complement] , i]
             nums_to_index[num] = i
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        method #1 for loop => O(n^2)
+        [2,7,15,11,15] => 22
+           ^i
+             ^j
+        Time complexity => O(n)
+
+        method #2 hash map
+        [2,7,15,11,15] => 22
+              ^
+        current + y = 22
+        y = 22-current
+
+        {
+            2:0,
+            7:1,
+        }
+        [1,2]
+        """
+
+        # dict storing hash map
+        result = {nums[0]:0}
+        # loop
+        for i in range(1,len(nums)):
+            # find the complement of the current number
+            complement = target - nums[i]
+            # check the complement with the key in the hash table
+            if complement in result:
+                # print([result[complement],i])
+                return [i,result[complement]]
+            result[nums[i]] = i
